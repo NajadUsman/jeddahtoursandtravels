b0dc2702ad4bd3d71510f95754b91757bc1734fc
NajadUsman <najadusman1896@gmail.com>
2025-02-02 21:38:26 +0530

    sevenfruits changed jeddahtoursandtravels

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sevenstarfruits'`, () => {
+  it(`should have as title 'jeddahtoursandtravels'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sevenstarfruits');
+    expect(app.title).toEqual('jeddahtoursandtravels');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sevenstarfruits app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('jeddahtoursandtravels app is running!');
   });
 });
